4f5ee309e362186fa2300156e69f822be5e81d94
HojinSohn <112189065+HojinSohn@users.noreply.github.com>
2023-08-28 01:20:23 -0400

    config change

--- venv/api/config.py
+++ venv/api/config.py
@@ -1,9 +1,14 @@
+import dotenv
 from app import app
 from flaskext.mysql import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Thsghwls0414@'
-app.config['MYSQL_DATABASE_DB'] = 'mytasks'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = os.getenv("MYSQL_DATABASE_USER")
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv("MYSQL_DATABASE_PASSWORD")
+app.config['MYSQL_DATABASE_DB'] = os.getenv("MYSQL_DATABASE_DB")
+app.config['MYSQL_DATABASE_HOST'] = os.getenv("MYSQL_DATABASE_HOST")
 mysql.init_app(app)
